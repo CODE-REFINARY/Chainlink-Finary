88a0cea1a47a1183de9df63297b534fe7d16752d
look-its-ashton <gtdubinin@gmail.com>
2023-01-05 13:40:09 -0800

    Added Preliminary Models for DB

--- Patchwork/models.py
+++ Patchwork/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Chainlink(models.Model):
+    title = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
