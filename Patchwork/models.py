26a471d944122792556aab9d29934039c97b3152
look-its-ashton <gtdubinin@gmail.com>
2023-01-05 22:09:28 -0800

    First Iteration of Chainlink Model Defined

--- Patchwork/models.py
+++ Patchwork/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.utils import timezone
 
 class Chainlink(models.Model):
-    title = models.CharField(max_length=200)
-    created_date = models.DateTimeField(default=timezone.now)
-    text = models.TextField()
+    title = models.CharField(max_length=200) # Header element for this chainlink
+    public = models.BooleanField(default=False) # Indicate whether this chainlink will be shareable
+    created_date = models.DateTimeField(default=timezone.now) # Creation date for this chainlink. May or may not be visible
+    text = models.TextField(max_length=3000, blank=True, default=str) # Text that will appear within <p> tags
+    bulleted_list = models.fields.json.JSONField(default=str, blank=True) # JSON formatted data that will appear as a bulleted lists with links and/or plaintext
+    media_gallery = models.fields.json.JSONField(default=str, blank=True) # JSON formatted data that will appear as a grid of images 
 
     def __str__(self):
         return self.title
---
+++ PostgreSQL Commands.md
@@ -0,0 +1,3 @@
+# Useful Posgre SQL Commands
+
+Show tables from the database: \dt
