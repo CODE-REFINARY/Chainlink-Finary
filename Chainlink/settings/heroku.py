561a35e50f04b2d21924149183b0873506f03be1
look-its-ashton <gtdubinin@gmail.com>
2023-03-27 08:47:41 -0700

    change db_key to db_password

--- Chainlink/settings/heroku.py
+++ Chainlink/settings/heroku.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
-        'PASSWORD': config('DB_KEY'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
     },
