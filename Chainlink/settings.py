fdb51f32c7d68a4593f3e0682cae10e387fc5257
look-its-ashton <78783142+look-its-ashton@users.noreply.github.com>
2023-06-30 17:15:10 -0700

    fix static issue

--- Chainlink/settings.py
+++ Chainlink/settings.py
@@ -15,7 +15,7 @@ from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
