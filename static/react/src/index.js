17b82b4d603228a7fb3634261cd61b0c76868950
look-its-ashton <78783142+look-its-ashton@users.noreply.github.com>
2023-08-03 13:13:25 -0700

    Fixed Webpack Container Bug

--- static/react/src/index.js
+++ static/react/src/index.js
@@ -9,7 +9,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>This is the CULT</h1>
+        <h1>This is the OC CULT</h1>
       </div>
     );
   }
